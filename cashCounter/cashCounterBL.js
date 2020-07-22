561d608cef11e8560bf73bf454816a8b7661f89c
DishonJP <dishonjp11@gmail.com>
2020-07-22 15:32:30 +0530

    [Dishon].add error handling in cashcounter

--- cashCounter/cashCounterBL.js
+++ cashCounter/cashCounterBL.js
@@ -3,27 +3,30 @@ const pattern = new RegExp("(1|2|3)");
 
 const Queue = require('./Queue');
 const bank = (name) => {
-    const queue = new Queue();
-    queue.enQueue(name)
-    while (!queue.isEmpty()) {
-        const userStatus = readline.questionInt("Do you like withdraw(1) or Deposit(2) or exit(3) : ");
-        if (pattern.test(userStatus)) {
-            switch (userStatus) {
-                case 1:
-                    const amount = readline.questionInt(`Hi ${queue.getUser()} , enter the amount to withdraw : `);
-                    console.log(`${queue.withdraw(amount)} : balance ${queue.getBalance()}`);
-                    break;
-                case 2:
-                    const depoAmount = readline.questionInt(`Hi ${queue.getUser()},Enter the amount to deposit : `);
-                    console.log(`${queue.deposit(depoAmount)} : balance ${queue.getBalance()}`);
-                    break;
-                default:
-                    console.log("transaction cancelled");
-                    queue.deQueue()
-                    break;
+    try {
+        const queue = new Queue();
+        queue.enQueue(name)
+        while (!queue.isEmpty()) {
+            const userStatus = readline.questionInt("Do you like withdraw(1) or Deposit(2) or exit(3) : ");
+            if (pattern.test(userStatus)) {
+                switch (userStatus) {
+                    case 1:
+                        const amount = readline.questionInt(`Hi ${queue.getUser()} , enter the amount to withdraw : `);
+                        console.log(`${queue.withdraw(amount)} : balance ${queue.getBalance()}`);
+                        break;
+                    case 2:
+                        const depoAmount = readline.questionInt(`Hi ${queue.getUser()},Enter the amount to deposit : `);
+                        console.log(`${queue.deposit(depoAmount)} : balance ${queue.getBalance()}`);
+                        break;
+                    default:
+                        console.log("transaction end");
+                        queue.deQueue()
+                        break;
+                }
             }
         }
-
+    } catch (error) {
+        console.log(error);
     }
 }
 
